1dfd9bf82da2959cfd29b65c35bf1c3ea82ee714
袁硕 <2755453130@qq.com>
2024-08-18 22:01:04 +0800

    1s

---
+++ 1s/Hardware - 副本/Buzzer.h
@@ -0,0 +1,9 @@
+#ifndef __BUZZER_H
+#define __BUZZER_H
+
+void Buzzer_init(void);
+void Buzzer_ON(void);
+void Buzzer_TURN(void);
+void Buzzer_OFF(void);
+
+#endif
---
+++ 1s/Hardware - 副本/Encoder.h
@@ -0,0 +1,7 @@
+#ifndef __ENCODER_H
+#define __ENCODER_H
+
+int16_t Encoder_Get(void);
+void Encoder_Init(void);
+	
+#endif
